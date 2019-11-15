52d06d90c02549705dfe67a0b1f1cb96c2da3e27
Walter Lim <waltissomewhere@gmail.com>
2019-11-16 10:27:17 +1300

    lol

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+
+const Header = styled.div`
+
+` // end header
+
+const MapContainer = styled.div`
+
+` // end MapContainer
+
+const Footer = styled.Footer`
+
+` //end footer
+
+const Index = () => (
+    <div>
+      <p>Hello fren</p>
+    </div>
+  );
+  
+  export default Index;
