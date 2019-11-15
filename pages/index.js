3e9f72a0770754bc3acddedeb1bde7ea39bfff34
Walter Lim <waltissomewhere@gmail.com>
2019-11-16 10:41:53 +1300

    initial scaffolding

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,40 @@
+import styled from 'styled-components'
+
+
+const Page = styled.div`
+
+`
 
 const Header = styled.div`
-
+background-color: #2c384c;
+height: 6rem;
+width: 100vw;
 ` // end header
 
 const MapContainer = styled.div`
 
 ` // end MapContainer
 
-const Footer = styled.Footer`
+const Footer = styled.div`
 
 ` //end footer
 
 const Index = () => (
     <div>
-      <p>Hello fren</p>
+        <style jsx global>{`
+        body { 
+        margin: 0;
+      }
+            `}</style>
+      <Header>
+        
+      </Header>
+      <MapContainer>
+
+      </MapContainer>
+      <Footer>
+
+      </Footer>
     </div>
   );
   
