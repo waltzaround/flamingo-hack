121acd2c2d4215503d1767204276f27e51e7ccaa
Aidan Harris <aidanharris1001@gmail.com>
2019-11-16 13:23:33 +1300

    Some static wave data is now being displayed

--- pages/googleMaps.js
+++ pages/googleMaps.js
@@ -1,26 +1,33 @@
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react'
 import React from 'react'
+import Scooters from '../wavescootersauckland'
 export class MapContainer extends React.Component {
   render() {
+    let coordinates = [];
+    for(let scootId in Scooters.birds) {
+      let scooter = Scooters.birds[scootId];
+        coordinates[scootId] =
+          <Marker name='Current location'
+            position= {{
+              lat: scooter.location.latitude,
+              lng: scooter.location.longitude 
+            }}
+          />
+    }
     return (
-    <Map 
-      google={this.props.google} 
-      zoom={14}
-      initialCenter={{
-        lat: -36.85,
-        lng: 174.75 
-      }}
-    >
-    <Marker name={'Current location'}
-      position= {{
-        lat: -36.85,
-        lng: 174.75 
-      }}
-    />
-
-    </Map>)
+      <Map 
+        google={this.props.google} 
+        zoom={14}
+        initialCenter={{
+          lat: -36.85,
+          lng: 174.75 
+        }}
+      >
+        {coordinates}
+      </Map>
+    )
+    }
   }
-}
 
 export default GoogleApiWrapper({
   apiKey: ''
