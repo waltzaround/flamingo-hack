2bc6ab1ae90eb6c85472b2fddb6ae64affedd0d1
Monique Warrington <niquewarrington@gmail.com>
2019-11-17 10:21:47 +1300

    essential components. Needs all the styling

---
+++ components/FeedbackHeader.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const FeedbackHeader = () => (
+  <div>
+    <div>
+      <h1>How did you enjoy your ride?</h1>
+    </div>
+    <div>
+      <img src="./greystar.png" style={{ "maxWidth": "64px" }} />
+      <img src="./greystar.png" style={{ "maxWidth": "64px" }} />
+      <img src="./greystar.png" style={{ "maxWidth": "64px" }} />
+      <img src="./greystar.png" style={{ "maxWidth": "64px" }} />
+      <img src="./greystar.png" style={{ "maxWidth": "64px" }} />
+    </div>
+  </div>
+)
+export default FeedbackHeader
---
+++ components/FeedbackInput.js
@@ -0,0 +1,12 @@
+const FeedbackInput = () => (
+  <div>
+    <div>
+      <h1>Tell us more (optional)</h1>
+    </div>
+    <div>
+      <input placeholder="Enter comments" />
+    </div>
+  </div>
+)
+
+export default FeedbackInput
